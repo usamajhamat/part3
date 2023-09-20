d355e78be0b508c3068d1fb6b80bfec8bb20f496
Muhammad Usama <usamajhamat111@gmail.com>
2023-09-20 00:16:52 -0700

    done

---
+++ index.js
@@ -0,0 +1,82 @@
+
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+let persons = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+
+const generateRandomId = () => {
+    return Math.floor(Math.random() * 1000000)
+}
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(`Phonebook has info for ${persons.length} people <br> ${new Date()}`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({ error: 'Name and number are required' })
+    }
+
+    const newPerson = {
+        id: generateRandomId(),
+        name: body.name,
+        number: body.number,
+    }
+
+    persons = persons.concat(newPerson)
+    response.json(newPerson)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
